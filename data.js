9c92eee4d5ef57dfcd7a414c371a280c1380faab
ReshmaGajananKeni <121846539+ReshmaGajananKeni@users.noreply.github.com>
2023-05-23 21:17:32 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,58 @@
+import burger from "../images/burger.jpeg";
+import pizza from "../images/pizza.jpeg";
+import momo from "../images/momo.jpeg";
+import nudles from "../images/nudles.jpeg";
+import drinks from "../images/drinks.jpeg";
+import crispi from "../images/crispi.jpeg";
+
+export const MenuList = [
+  {
+    name: "Burger",
+    description:
+      "a spiced mix of meat with a variety of components to create a number of highly tasty and unique flavor combinations",
+    image: burger,
+    price: 380,
+
+  },
+  
+  {
+    name: "Pizza",
+    description:
+      "It is a combination of chinease and mexican tatse bread made from maida",
+    image: pizza,
+    price: 250,
+ 
+  },
+  {
+    name: "Chicken Momos",
+    description:
+      "The Taste of Momos can change your mood Quickly! The way Momos is prepared or served affects the flavor and texture of the fish. The flavor of fish can range from mild and sweet to rich and strong",
+    image: momo,
+    price: 350,
+
+  },
+  {
+    name: "Chicken Crispi",
+    description:
+      "Chicken crispi is made with chicken, spices, herbs, onions and tomatoes. Serve with rice, naan, roti or paratha.",
+    image: crispi,
+    price: 470,
+
+  },
+  {
+    name: "Noodles",
+    description:
+      "Noodles is a popular and delicious snack where (Indian cottage cheese cubes) are marinated in a spiced yogurt-based .",
+    image: nudles,
+    price: 250,
+   
+  },
+  {
+    name: "Drinks",
+    description: "Serve a tasty delicius food with refreshing Drinks!!",
+    image: drinks,
+    price: 150,
+ 
+  },
+];
+
